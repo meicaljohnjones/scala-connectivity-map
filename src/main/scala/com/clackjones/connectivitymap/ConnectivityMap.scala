5dbb06c983de198d0f2af8abcc9838a53e0bbeb9
Mike Jones <mjclackjones@gmail.com>
2015-05-20 16:25:31 +0100

    Add test for connectionScores function and write corresponding function

--- src/main/scala/com/clackjones/connectivitymap/ConnectivityMap.scala
+++ src/main/scala/com/clackjones/connectivitymap/ConnectivityMap.scala
@@ -2,6 +2,12 @@ package com.clackjones.connectivitymap
 
 object ConnectivityMap {
 
+  /**
+   * Calculate the connection strength for one
+   * @param referenceProfile a ReferenceProfile object
+   * @param querySignature
+   * @return a tuple containing the name of the reference profile and its connection strength
+   */
   def connectionStrength(referenceProfile: ReferenceProfile, querySignature: Map[String, Int]): (String, Int) = {
     val keys = querySignature.keys.toSet
 
@@ -18,6 +24,21 @@ object ConnectivityMap {
     (referenceProfile.name, strengths.toList.sum)
   }
 
+
+  def connectionScores(profiles: Set[ReferenceProfile], querySignature: Map[String, Int],
+                       connectionStrength: (ReferenceProfile, Map[String, Int]) => (String, Int)): Set[(String, Float)] = {
+
+    val connectionStrengths = profiles map (connectionStrength(_, querySignature))
+
+    def maxStrengthImpl(cs1: (String, Int), cs2: (String, Int)) = if (cs1._2 > cs2._2) cs1 else cs2
+    val maxConnectionStrength: Float = (connectionStrengths reduceLeft (maxStrengthImpl))._2.toFloat
+
+    def connectionStrengthToScore (strengthTuple: (String, Int), maxScore: Float): (String, Float) = {
+      (strengthTuple._1, strengthTuple._2 /  maxScore)
+    }
+
+    connectionStrengths map (connectionStrengthToScore(_, maxConnectionStrength))
+  }
 
 
 }
--- src/test/scala/com/clackjones/connectivitymap/ConnectivityMapSpec.scala
+++ src/test/scala/com/clackjones/connectivitymap/ConnectivityMapSpec.scala
@@ -8,4 +8,16 @@ class ConnectivityMapSpec extends UnitSpec {
 
     connectionStrength(expressionProfile, querySignature) shouldBe (expressionProfile.name, 2)
   }
+
+  "Passing a single gene expression profile and query signature to connectionScores" should
+    "return one connection strength tuple with score 1.0" in {
+
+    val expressionProfile = new ReferenceProfile(name = "profile1", Map("gene1" -> 1, "gene2" -> -5, "gene3" -> 7))
+    val querySignature : Map[String, Int] = Map("gene2" -> 1, "gene3" -> 1)
+
+    def mockConnectionStrength(prof: ReferenceProfile, query: Map[String, Int]): (String, Int) = ("resultprofile", 50)
+
+    connectionScores(Set(expressionProfile), querySignature, mockConnectionStrength) shouldBe Set(("resultprofile", 1f))
+  }
+
 }
