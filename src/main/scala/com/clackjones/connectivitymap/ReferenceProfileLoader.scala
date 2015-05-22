38bede15c1bd245eb3b22266413d4ad0347b53ed
Mike Jones <mjclackjones@gmail.com>
2015-05-22 16:52:55 +0100

    Implement ReferenceProfileFileLoader.loadReferenceProfile (No tests yet)

---
+++ src/main/scala/com/clackjones/connectivitymap/ReferenceProfileLoader.scala
@@ -0,0 +1,62 @@
+package com.clackjones.connectivitymap
+
+import java.util.regex.Pattern
+
+trait ReferenceProfileLoader {
+
+  /**
+   * Loads an individual ReferenceProfile
+   * @param path path to the individual Reference Profile
+   * @return a ReferenceProfile object
+   */
+  def loadReferenceProfile(path: String): ReferenceProfile
+
+  /**
+   * Loads a set of ReferenceProfiles
+   * @param path path to the set of Reference Profiles
+   * @return
+   */
+  def loadReferenceProfiles(path: String): Set[ReferenceProfile]
+}
+
+
+import scala.io.Source
+
+object ReferenceProfileFileLoader extends ReferenceProfileLoader {
+  val whitespacePattern = Pattern.compile("\\s+")
+  /**
+   * Loads an individual ReferenceProfile
+   * @param path path to the individual Reference Profile
+   * @return a ReferenceProfile object
+   */
+  override def loadReferenceProfile(path: String): ReferenceProfile = {
+    val srcFile = Source.fromFile(path).getLines()
+
+    val geneFoldChange = (srcFile map (line => splitLine(line))).toMap
+    val name = path match {
+      case s if s.contains("/") => path.substring(s.lastIndexOf("/") + 1)
+      case _ => path
+    }
+
+    new ReferenceProfile(name, geneFoldChange)
+  }
+
+  /**
+   * Loads a set of ReferenceProfiles
+   * @param path path to the set of Reference Profiles
+   * @return
+   */
+  override def loadReferenceProfiles(path: String): Set[ReferenceProfile] = {
+    // TODO
+    throw new NotImplementedError("loadReferenceProfiles not yet implemented")
+  }
+
+  private def splitLine(line: String): (String, Int) = {
+    val splitLine = whitespacePattern.split(line.trim())
+
+    val geneName = splitLine(0)
+    val geneStrength = splitLine(1).toInt
+
+    (geneName, geneStrength)
+  }
+}
