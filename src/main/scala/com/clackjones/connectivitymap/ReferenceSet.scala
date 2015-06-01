6c27e49f117d0dfa7679b098fdfaceeaf0b09170
Mike Jones <mjclackjones@gmail.com>
2015-06-01 22:18:31 +0100

    Add descrition for ReferenceSet class and add two unimplemented functions for retrieving profiles from the ReferenceSet along with two failing tests.

--- src/main/scala/com/clackjones/connectivitymap/ReferenceSet.scala
+++ src/main/scala/com/clackjones/connectivitymap/ReferenceSet.scala
@@ -1,3 +1,31 @@
 package com.clackjones.connectivitymap
 
-class ReferenceSet(val name: String, val filenames: Iterable[String])
+/**
+ * A class representing a ReferenceSet which contains a collection of
+ * <code>filenames</code> representing the paths to each reference profile
+ * file for this ReferenceSet
+ *
+ * The class also provides methods to load the ReferenceProfiles from this set
+ * and also to instantiate a ReferenceProfile with the average fold change of
+ * the different Probe IDs of each profile in the set.
+ *
+ * @param name The name to give this ReferenceSet
+ * @param filenames A collection of absolute paths to the reference profiles of this ReferenceSet
+ */
+class ReferenceSet(val name: String, val filenames: Iterable[String]) {
+
+  def retrieveReferenceProfiles() : Iterable[ReferenceProfile] = {
+    throw new UnsupportedOperationException("retrieveReferenceProfiles not yet implemented")
+  }
+
+  /**
+   * A function which loads all ReferenceProfiles from file in this ReferenceSet and
+   * creates a ReferenceProfile which has the average (function specified by
+   * user) fold change for each Probe ID
+   * @param avgFun A function that describes how the average of each Gene ID is calculated (e.g. mean, median...etc.)
+   * @return A ReferenceProfile with the average fold change for each gene ID from all reference profiles int the set
+   */
+  def retrieveAverageReferenceProfile(avgFun: Set[Float] => Float) : ReferenceProfile = {
+    throw new UnsupportedOperationException("retrieveAverageReferenceProfile not yet implemented")
+  }
+}
---
+++ src/test/scala/com/clackjones/connectivitymap/ReferenceSetSpec.scala
@@ -0,0 +1,13 @@
+package com.clackjones.connectivitymap
+
+class ReferenceSetSpec extends UnitSpec {
+
+  "retrieveReferenceProfiles" should "retrieve a set of ReferenceProfiles for a given ReferenceSet object" in {
+    fail("not yet implemented")
+  }
+
+  "retrieveAverageReferenceProfile" should "retrieve an average ReferenceProfiles for a given ReferenceSet object" in {
+    fail("not yet implemented")
+  }
+
+}
