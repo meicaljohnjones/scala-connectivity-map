6187fba300dd20ba8aaddc0b54eb1924a1f06d35
Mike Jones <mjclackjones@gmail.com>
2015-06-11 19:04:56 +0100

    Write FileBasedReferenceSetProviderComponent

---
+++ src/main/scala/com/clackjones/connectivitymap/service/ReferenceSetProviderComponent.scala
@@ -0,0 +1,36 @@
+package com.clackjones.connectivitymap.service
+
+import java.io.File
+
+import com.clackjones.connectivitymap._
+import com.clackjones.connectivitymap.referenceprofile.{ReferenceSetCreatorByDrugDoseAndCellLineComponent, ReferenceSetCreatorComponent}
+
+trait ReferenceSetProviderComponent {
+  def referenceSetProvider : ReferenceSetProvider
+
+  trait ReferenceSetProvider {
+    def findAll() : Iterable[ReferenceSet]
+  }
+
+  case class ReferenceSet(val name: String, val filenames: Iterable[String])
+}
+
+trait FileBasedReferenceSetProviderComponent extends ReferenceSetProviderComponent {
+  this: ReferenceSetCreatorComponent =>
+  val referenceSetProvider = new FileBasedReferenceSetProvider
+
+  class FileBasedReferenceSetProvider extends ReferenceSetProvider {
+    val reffiles : File = new File(getClass().getResource(config("reffileLocation")).toURI())
+
+    override def findAll(): Iterable[ReferenceSet] = {
+      val pathToFiles = reffiles.getAbsolutePath()
+      val filenames = reffiles.list()
+
+      val refsets : Iterable[ReferenceSet] = referenceSetCreator.createReferenceSets(pathToFiles, filenames) map {
+        refset => ReferenceSet(refset.name, refset.filenames)
+      }
+
+      refsets
+    }
+  }
+}
