93d78aecf386dd07c1957f6e3e17973a1e6b6f91
Mike Jones <mjclackjones@gmail.com>
2015-06-18 04:01:51 +0100

    Remove unused imports

--- src/main/scala/com/clackjones/connectivitymap/rest/MainController.scala
+++ src/main/scala/com/clackjones/connectivitymap/rest/MainController.scala
@@ -1,13 +1,8 @@
 package com.clackjones.connectivitymap.rest
 
-import org.scalatra.{NotFound, Ok, ScalatraServlet}
+import org.scalatra.ScalatraServlet
 import org.scalatra.scalate.ScalateSupport
-
-// JSON-related libraries
 import org.json4s.{DefaultFormats, Formats}
-
-// JSON handling support from Scalatra
-import org.scalatra.json._
 
 class MainController extends ScalatraServlet with ScalateSupport {
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
