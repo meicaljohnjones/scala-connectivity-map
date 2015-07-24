e03121004f31c921e276e0672e32dcfe76424402
Mike Jones <mjclackjones@gmail.com>
2015-07-24 07:38:35 +0100

    Remove superfluous settings from SparkContext and change web port to 6789 for web console

--- src/main/scala/com/clackjones/connectivitymap/spark/SparkContextComponent.scala
+++ src/main/scala/com/clackjones/connectivitymap/spark/SparkContextComponent.scala
@@ -5,8 +5,6 @@ import org.apache.spark.{SparkConf, SparkContext}
 trait SparkContextComponent {
   private val conf = new SparkConf()
     .setAppName("Simple Application")
-    .setMaster("local[4]")
-    .set("spark.executor.memory", "4g")
   val sc : SparkContext =  new SparkContext(conf)
 
 }
