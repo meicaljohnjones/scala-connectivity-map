ffcdc51841c515e7a14e65ae0fa2ffb8546078a6
Mike Jones <mjclackjones@gmail.com>
2015-08-12 17:58:10 +0100

    Change default directory for reffiles to '.../reffiles' instead of '.../reffiles_subset'

--- src/main/scala/com/clackjones/connectivitymap/package.scala
+++ src/main/scala/com/clackjones/connectivitymap/package.scala
@@ -4,7 +4,7 @@ package object connectivitymap {
   type QuerySignatureMap = Map[String, Float]
 
   val config = Map(
-    "reffileLocation" -> "/home/mike/connectivity_map_resources/reffiles_subset",
+    "reffileLocation" -> "/home/mike/connectivity_map_resources/reffiles",
     "querySignatureLocation" -> "/home/mike/connectivity_map_resources/queries",
     "defaultSignatureCount" -> "30000"
   )
